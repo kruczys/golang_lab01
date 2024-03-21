4f0fee6cbdb33dc6d3efca544520c1f50f05b6f9
Konrad Kreczko <konradkreczko@gmail.com>
2024-03-21 13:20:53 +0100

    Started work on excercise 2

---
+++ lab02/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+)
+
+func factorial(number int) [1000]byte  {
+    var result_array [1000]byte
+    return result_array
+}
+
+func show_number(number_array [1000]byte){
+    return
+}
+
+func fib(number uint) uint {
+    return number
+}
+
+func main() {
+    fmt.Printf("%d\n", factorial(20))
+    fmt.Printf("%d\n", factorial(40))
+    fmt.Printf("%d\n", factorial(60))
+    fmt.Printf("%d\n", factorial(80))
+}
