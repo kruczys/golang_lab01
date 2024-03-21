1488e58135e13d34ab91e9912aaf1f00763f8da2
Konrad Kreczko <konradkreczko@gmail.com>
2024-03-21 12:28:06 +0100

    Last touchups

--- lab01/main.go
+++ lab01/main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-    game_rounds := 100
+    game_rounds := 1000000
     player_wins_staying := 0
     player_wins_switching := 0
+    is_staying := false
+    // is_staying := true
    
-    for i := 0; i <= game_rounds; i++ {
+    for i := 0; i < game_rounds; i++ {
         var single_round [3]int
         winning_box := rand.Intn(3)
         player_choice := rand.Intn(3)
@@ -28,6 +30,13 @@ func main() {
         }
     }
 
-    fmt.Println("wins by staying", player_wins_staying)
-    fmt.Println("wins by switching", player_wins_switching)
+    if is_staying {
+        fmt.Println("Player tactic was to stay")
+        fmt.Println("wins: ", player_wins_staying)
+        fmt.Println("loses: ", game_rounds - player_wins_staying)
+    } else {
+        fmt.Println("Player tactic was to switch")
+        fmt.Println("wins: ", player_wins_switching)
+        fmt.Println("loses: ", game_rounds - player_wins_switching)
+    }
 }
