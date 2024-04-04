797f9e84df3e3bc1ecfbe202d14b3c6dc573ab09
Konrad Kreczko <konradkreczko@gmail.com>
2024-04-04 13:11:05 +0200

    Created function for generating nicknames and asciicodes for further devlopment

--- lab02/main.go
+++ lab02/main.go
@@ -2,24 +2,43 @@ package main
 
 import (
     "fmt"
+    "math/big"
+    "strings"
 )
 
-func factorial(number int) [1000]byte  {
-    var result_array [1000]byte
-    return result_array
-}
-
-func show_number(number_array [1000]byte){
-    return
+func factorial(number int) *big.Int{
+    result := big.NewInt(1)
+    for i := 1; i < number; i++ {
+        result.Mul(result, big.NewInt(int64(i)))
+    }
+    return result
 }
 
 func fib(number uint) uint {
     return number
 }
 
+func containsSubstring(container, containee string) bool {
+    return strings.Contains(container, containee)
+}
+
+func generateNick(name, surname string) string {
+    return strings.ToLower(name[:3] + surname[1:4])
+}
+
+func generateAscii(nick string) [6]int {
+    var result [6]int
+    for i := 0; i <= 5; i++ {
+        result[i] = int(nick[i])
+    }
+    return result
+}
+
 func main() {
-    fmt.Printf("%d\n", factorial(20))
-    fmt.Printf("%d\n", factorial(40))
-    fmt.Printf("%d\n", factorial(60))
-    fmt.Printf("%d\n", factorial(80))
+    test := factorial(123)
+    fmt.Printf("%s\n", test.String())
+    fmt.Println(containsSubstring(test.String(), `000`))
+    nick := generateNick(`Konrad`, `Kreczko`)
+    fmt.Println(nick)
+    fmt.Println(generateAscii(nick))
 }
